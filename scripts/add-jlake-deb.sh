93c93dae34e28c1d811a559da88b6feea939da1d
Jim Lake <jim@blueskylabs.com>
2016-04-28 08:39:15 -0700

    deb add user

---
+++ scripts/add-jlake-deb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo adduser --disabled-password --gecos "" -q jlake
+sudo usermod -a -G admin jlake
+sudo mkdir ~jlake/.ssh
+sudo bash -c 'curl https://blueskylabs.com/jlake.pub > ~jlake/.ssh/authorized_keys'
+sudo chmod 755 ~jlake/.ssh
+sudo chmod 755 ~jlake/.ssh/authorized_keys
+sudo chown jlake:jlake ~jlake/.ssh
+sudo chown jlake:jlake ~jlake/.ssh/authorized_keys
+sudo bash -c 'echo "%admin ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/91-admin'
